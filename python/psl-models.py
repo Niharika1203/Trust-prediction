9fca6d754bee819fc550c33bc6e2d03409c12d8c
Niharika1203 <niharika.belle@gmail.com>
2020-09-21 15:02:00 -0700

    added data folds

--- python/psl-models.py
+++ python/psl-models.py
@@ -22,20 +22,19 @@ ADDITIONAL_PSL_OPTIONS = {
     'log4j.threshold': 'DEBUG'
 }
 
-SPLITS = [0]
-
+SPLITS = [0,1, 2,3,4,5,6,7]
 ADDITIONAL_CLI_OPTIONS = [
      '--postgres'
 ]
 
 def main():
     # model = Model(MODEL_NAME)
-    for data_split in SPLITS :
+    for data_fold in SPLITS :
         # predicate_dir = DATA_DIR + str(split) + "/eval"
         # print(predicate_dir)
-        models = [ "balance5" ]
-        # "balance5", "balance5_recip", "balance_extended", "balance_extended_recip",
-        #  "status" , "status_inv" , "personality", "cyclic_balanced" , "cyclic_bal_unbal"  ]
+        # models = ["balance5"]
+        models = [ "balance5", "balance5_recip", "balance_extended", "balance_extended_recip",
+          "status" , "status_inv" , "personality"] # "cyclic_balanced" , "cyclic_bal_unbal"  ]
 
         for model_name in models :
             model = makeModel(model_name)
@@ -68,16 +67,16 @@ def main():
                 print('   ' + str(rule))
 
             # Weight Learning
-            model = learn(model, str(data_split) , model_name)
+            model = learn(model, str(data_fold) , model_name)
 
             print('Learned Rules:')
             for rule in model.get_rules():
                 print('   ' + str(rule))
 
             # Inference
-            results = infer(model, str(data_split) , model_name)
-            write_results(results, model, model_name)
-            evalute(model, str(data_split) , model_name)
+            results = infer(model, str(data_fold) , model_name)
+            write_results(results, model, model_name, str(data_fold))
+            evalute(model, str(data_fold) , model_name)
 
 def makeModel(model_name, addPrior = True, square = True, sim = False):
     model = Model(model_name)
@@ -96,14 +95,14 @@ def makeModel(model_name, addPrior = True, square = True, sim = False):
 
     return model
 
-def add_learn_data(model,datasplit, model_name):
-    _add_data('learn', model, datasplit, model_name)
+def add_learn_data(model, data_fold, model_name):
+    _add_data('learn', model, data_fold, model_name)
 
-def add_eval_data(model,datasplit, model_name):
-    _add_data('eval', model,datasplit, model_name )
+def add_eval_data(model, data_fold, model_name):
+    _add_data('eval', model, data_fold, model_name )
 
-def _add_data(split, model, datasplit, model_name):
-    split_data_dir = os.path.join(DATA_DIR, datasplit, split)
+def _add_data(split, model, data_fold, model_name):
+    split_data_dir = os.path.join(DATA_DIR, data_fold, split)
     for predicate in model.get_predicates().values():
         predicate.clear_data()
 
@@ -123,18 +122,18 @@ def _add_data(split, model, datasplit, model_name):
     model.get_predicate('Trusts').add_data_file(Partition.TRUTH, path)
 
     if model_name == "personality":
-        path = os.path.join(split_data_dir, 'users.txt')
+        path = os.path.join(split_data_dir, 'trusting.txt')
         model.get_predicate('Trusting').add_data_file(Partition.TARGETS, path)
-        path = os.path.join(split_data_dir, 'usersTW.txt')
+        path = os.path.join(split_data_dir, 'trustworthy.txt')
         model.get_predicate('TrustWorthy').add_data_file(Partition.TARGETS, path)
 
-def learn(model, datasplit, model_name):
-    add_learn_data(model, datasplit, model_name)
+def learn(model, data_fold, model_name):
+    add_learn_data(model, data_fold, model_name)
     model.learn(additional_cli_optons = ADDITIONAL_CLI_OPTIONS, psl_config = ADDITIONAL_PSL_OPTIONS)
     return model
 
-def write_results(results, model, model_name):
-    out_dir = model_name + '/inferred-predicates'
+def write_results(results, model, model_name, data_fold):
+    out_dir = model_name + "/"+ data_fold + '/inferred-predicates'
     os.makedirs(out_dir, exist_ok = True)
 
     for predicate in model.get_predicates().values():
@@ -222,21 +221,21 @@ def personality_rules(model) :
     model.add_rule(Rule("1.0: Knows(A, B) & Prior('0') -> Trusts(A, B) ^2"))
     model.add_rule(Rule("1.0: Knows(A, B) & Trusts(A, B) -> Prior('0') ^2"))
 
-def infer(model, datasplit, model_name):
-    add_eval_data(model, datasplit, model_name)
+def infer(model, data_fold, model_name):
+    add_eval_data(model, data_fold, model_name)
     return model.infer(additional_cli_optons = ADDITIONAL_CLI_OPTIONS, psl_config = ADDITIONAL_PSL_OPTIONS)
 
 
-def evalute(model, datasplit, model_name):
-    inferred_direc = "/inferred-predicates/TRUSTS.txt"
+def evalute(model, data_fold, model_name):
+    inferred_direc = "/"+ str(data_fold) + "/inferred-predicates/TRUSTS.txt"
     models_direc = os.path.join(model_name)
-    truth_file = open(DATA_DIR + str(datasplit) + "/eval/trusts_truth.txt", "r+")
+    truth_file = open(DATA_DIR + str(data_fold) + "/eval/trusts_truth.txt", "r+")
     y_true_lines = truth_file.readlines()
-    file_pred = open(models_direc+inferred_direc, "r+")
+    file_pred = open(models_direc + inferred_direc, "r+")
     y_pred_lines = file_pred.readlines()
 
     def readfile(y_true_lines, y_pred_lines) :
-        y_true = [] # y_obs
+        y_true = []
         y_pred = []
         for line in y_true_lines :
             trustee , trusting, value = line.split()
@@ -279,7 +278,10 @@ def evalute(model, datasplit, model_name):
         aupr =  auc(recal, precis)
         return aupr
 
-    eval_file = open(models_direc + "/evaluation_result.txt", "w+")
+    evalDir = models_direc +"/" + str(data_fold)
+    os.makedirs(evalDir, exist_ok = True)
+    eval_file = open(evalDir + "/evaluation_result.txt", "w+")
+
     eval_file.write("Results for "+ model_name + "\n")
     # print("Results for PSL-BALANCE Model with 16 rules and priors.")
     obsArr, predArr = readfile(y_true_lines, y_pred_lines)
